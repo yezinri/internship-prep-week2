996441cff5dc7d9910363a1bc8b4b317a3257015
yejin <yejinlee0707@gmail.com>
2023-07-19 13:45:34 +0900

    practice

---
+++ day1/data_types.py
@@ -0,0 +1,18 @@
+number_list = [1, 2, 3, 4, 5]
+names = ["Lukas", "David", "George", "Lee"]
+
+random_list = [3, 3.9, "Python", False, True, [1, 2, 3]]
+random_list = [1, 2, 3, 4, 5]
+
+print(random_list)
+
+for element in random_list:
+    print(type(element))
+
+# TUPLE
+my_list = [1, 2, 3, 4, 5]
+my_list[2] = 50
+print(my_list[2])
+
+my_tuple = (1, 2, 3.5, "Hello", False)
+print(my_tuple[2])
---
+++ day1/practice1.py
@@ -0,0 +1,23 @@
+str = 'Dog is Love'
+
+# 1.
+print(str[-4:])
+
+# 2.
+new_str = str.replace('Dog', 'Cat')
+print(new_str)
+
+# 3.
+la = 'lalalalalalalalalala'
+print(len(la))
+print(la.count('a'))
+
+# 4.
+phone_num = '010-1234-5678'
+new_phone_num = phone_num.replace('-', '')
+print(new_phone_num)
+
+# 5.
+a = "hello"
+b = "python"
+print(a+'!', b)
